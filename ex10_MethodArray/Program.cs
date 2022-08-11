278c707521f029ccf24ed98ff8f1b0d38b6ce261
OlegSam2 <doorsmania@mail.ru>
2022-08-11 18:42:42 +0300

    add ex10_MethodArray find element array

---
+++ ex10_MethodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array ={15, 21, 39, 12, 23, 33, 13, 23, 33};
+
+int n = array.Length; // длина (количество элементов массива
+int find = 23; // поиск индекса по значению 23
+
+int index = 0; // счетчик
+
+while (index < n)
+{
+        if(array[index]==find)
+        {
+            Console.WriteLine(index);
+            break;
+        }
+//     index =  index +1;
+        index++;
+}
